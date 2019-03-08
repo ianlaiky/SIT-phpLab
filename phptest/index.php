a3aa2a1240ae87c10967204b261d9c887f0e8024
Ian <152772a@mymail.nyp.edu.sg>
2019-03-08 09:24:47 +0800

    i

--- phptest/index.php
+++ phptest/index.php
@@ -2,33 +2,145 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>te</title>
+    <title>Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link href="css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="css/css.css" rel="stylesheet"/>
 
-    <link href="newcss.css" rel="stylesheet"/>
+
 </head>
 <body>
-<?php
-$m = 5;
-$n = 10;
-$sum_of_factorials = calculate_sum_of_factorials ($m, $n);
-echo "m! + n! = " . $sum_of_factorials;
-function calculate_sum_of_factorials ($argument1, $argument2) {
-    $factorial1 = calculate_factorial ($argument1);
-    $factorial2 = calculate_factorial ($argument2);
-    $result = calculate_sum ($factorial1, $factorial2);
-    return $result;
-}
-function calculate_factorial ($argument) {
-    $factorial_result = 1;
 
-    for ($i=1; $i<=$argument; $i++) {
-        $factorial_result *= $i;
-    }
-    return $factorial_result;
-}
-function calculate_sum ($argument1, $argument2) {
-    return $argument1 + $argument2;
-}
-?>
+
+<nav class="navbar navbar-default justify-content-between">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Resorts World Sentosa</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-star"></span> Favorite Attraction</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-lock"></span> Block Tickets</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-star"></span> About RWS</a></li>
+
+
+        </ul>
+        <div class="navbar-form pull-right">
+
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+
+        </div>
+    </div>
+</nav>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="jumbotron">
+            <h1 class="display-4">Resorts World Sentosa Singapore</h1>
+            <p class="lead">We offers attractions that leave you wanting more...</p>
+            <p class="lead">
+                <a class="btn btn-primary btn-lg" href="#" role="button">Start Now!</a>
+            </p>
+        </div>
+
+
+    </div>
+
+</div>
+
+<div class="container">
+    <div class="row row-eq-height">
+
+        <table class="table">
+
+
+        </table>
+        <div class="col-lg-4 col-md-6 col-sm-12">
+            <div class="panel panel-default">
+                <div class="panel-body"><img src="images/3.jpeg" class="img-circle img-thumbnail img-responsive">
+                    <h2>
+                        Universal Studio
+                    </h2>
+                    <p>
+                        kdbcfshfjldsflidsdfsdfds
+                        sfdfdsfdsfdsfsdfdsfdsdfkghdfjhdfhgdfjghdfjdfjkhgjhdfjk
+                        fdsfds
+                        fdsdbcfshfjldsflidsdfsdfds
+                        sfdfdsfdsfdsfsdfdsfdsdfkghdfjhdfhgdfjghdfjdfjkhgjhdfjk
+                        fdsfds
+
+                    </p></div>
+                <div class="panel-footer">
+                    <button type="button" class="btn btn-success align-bottom">Book Now @$320</button>
+                </div>
+            </div>
+
+        </div>
+
+
+        <div class="col-lg-4 col-md-6 col-sm-12">
+
+
+            <div class="panel panel-default">
+                <div class="panel-body"><img src="images/1.jpeg" class="img-circle img-thumbnail img-responsive">
+
+                    <h2>
+                        Adventure Cove
+                    </h2>
+                    <p>
+                        kdbcfshfjldsflidsdfsdfds
+                        sfdfdsfds
+
+                    </p></div>
+                <div class="panel-footer">
+                    <button type="button" class="btn btn-danger align-bottom">Book Now @$320</button>
+                </div>
+            </div>
+
+
+        </div>
+
+
+        <div class="col-lg-4 col-md-6 col-sm-12">
+
+            <div class="panel panel-default">
+                <div class="panel-body">P<img src="images/2.jpeg" class="img-circle img-thumbnail img-responsive">
+                    <h2>
+                        Underwater World
+                    </h2>
+
+                    <p>
+                        kdbcfshfjldsflidsdfsdfds
+                        sfdfdsfds
+                    </p></div>
+                <div class="panel-footer">
+                    <button type="button" class="btn btn-info align-bottom">Book Now @$320</button>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+
+</div>
+
+<footer>
+
+    <div class="panel panel-info">
+        <div class="panel-heading text-center">©2015 Singapore Insitute Of Technology, ICT 1004</div>
+
+    </div>
+</footer>
+
+
+<script
+        src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js">
+</script>
+<script src="js/bootstrap.min.js"></script>
+
 </body>
 </html>
